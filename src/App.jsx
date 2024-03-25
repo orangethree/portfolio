651e1c3ddda2184819de7daa1ab979fe49d003d6
Andy Tran <TylerT124@yahoo.com>
2024-03-25 00:01:47 +0000

    "Modified App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Andy Tran",
+  title: "Student, UC Davis",
+  email: "TylerT124@yahoo.com",
+  gitHub: "https://github.com/orangethree",
+  instagram: "@tandyran",
+  linkedIn: "https://www.linkedin.com/in/andy-tran-760077267/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
